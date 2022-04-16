d7b77ee22f86f9b6e3f5cf9b601e44aedc5f63e2
Unknown-fakh <fakhruddindotcom@gmail.com>
2022-04-16 09:08:15 +0700

    aboutus

---
+++ resources/views/public/about/aboutus.blade.php
@@ -0,0 +1,77 @@
+<x-main >
+    <div >
+        <div class=""><img src="{{ url('/image/aboutus/bannerAboutus1.jpg') }}" alt="ua1"></div>
+    </div>
+
+    <div class="container py-3">
+        <div class="text-center my-4">
+            <h3 class="text-5xl">welcome</h3>
+            <p class="text-lg my-8">Urban Athletes adalah sebuah tempat bagi mereka yang ingin berinspirasi untuk hidup sehat.</p>
+            <p class="text-lg">
+                percaya bahwa kesehatan yang terlihat dari luar berasal dari kesehatan yang telah diolah 
+                dari dalam. Seiring berkembangnya zaman dan kesibukan yang terus meningkat, 
+                kami melihat semakin banyak orang yang tidak peduli dengan kesehatan. 
+                Hal ini menjadikan motivasi bagi kami untuk memberikan tempat khusus bagi mereka yang 
+                ingin menjadikan hidup sehat sebagai gaya hidup dengan metode yang tepat dan fun.</p>
+        </div>
+
+        <div class="my-8"><img src="{{ url('/image/aboutus/bannerAboutus2.jpg') }}" alt="ua2"></div>
+
+        <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-2">
+            <div><img src="{{ url('/image/aboutus/fotoAboutus1.jpg') }}"></div>
+            <div><img src="{{ url('/image/aboutus/fotoAboutus2.jpg') }}"></div>
+            <div><img src="{{ url('/image/aboutus/fotoAboutus3.jpg') }}"></div>
+            <div><img src="{{ url('/image/aboutus/fotoAboutus4.jpg') }}"></div>
+        </div>
+
+        <div>
+            <h4 class="text-5xl text-center my-4">Kenapa Urban Athletes?</h4>
+        </div>
+
+        <div>
+            <div class="grid md:grid-cols-2 lg:grid-cols-4 text-black">
+                <div class="flex justify-center place-items-center text-center"><a class=""
+                href="https://www.facebook.com/urbanathletesindo/" target="_blank" rel="Facebook">
+                <i class="fa-solid fa-user text-5xl"></i>
+                <p class="text-2xl"><span class="italic"> Personal Trainer </span> yang telah tersertifikasi</p>
+                <p>Trainer profesional kami akan membantu Anda secara tepat dan cepat dengan standarisasi yang telah ditentukan.
+                     Konsultasi fitness dan kesehatan juga dapat dilakukan dengan para trainer kami.</p>
+                </a>
+            </div>
+                <div class="flex justify-center place-items-center text-center"><a class=""
+                href="https://www.instagram.com/urban.athletes/" target="_blank" rel="instagram">
+                <i class="fa-solid fa-thumbs-up text-5xl"></i>
+                <p class="text-2xl">Akses ke lebih dari 500+ kelas</p>
+                <p>Fasilitas unggulan kami yang lain adalah tersedianya kelas-kelas yang dapat diikuti oleh para member Urban Athletes, 
+                    dipimpin oleh pelatih-pelatih berpengalaman di bidangnya.</p>
+                </a></div>
+                <div class="flex justify-center place-items-center text-center"><a class=""
+                href="https://www.youtube.com/channel/UCLV--bH8TFFYEf3Nz7Hv2vA" target="_blank" rel="youtube">
+                <i class="fa-solid fa-star text-5xl"></i>
+                <p class="text-2xl">Fasilitas fitness yang telah terstandarisasi</p>
+                <p>Keamanan bagi semua member kami tetap menjadi prioritas utama Urban Athletes. Kesehatan tetap nomor satu, 
+                    tetapi apa artinya tanpa keselamatan yang terjamin.</p>
+                </a></div>
+                <div class="flex justify-center place-items-center text-center"><a class=""
+                href="https://www.youtube.com/channel/UCLV--bH8TFFYEf3Nz7Hv2vA" target="_blank" rel="youtube">
+                <i class="fa-solid fa-heart text-5xl"></i>
+                <p class="text-2xl">Suasana yang ramah</p>
+                <p>Tidak hanya sebagai tempat berolahraga, kami ingin Urban Athletes menjadi rumah yang nyaman bagi semua member kami. 
+                    Para staff dan seluruh keluarga Urban Athletes akan menjadi teman terbaik Anda.</p>
+                </a></div>
+              </div>
+
+              <div>
+                <h3 class="text-5xl text-center my-8 text-lime-500">MEET OUR SUPER HEROES!</h3>
+            </div>
+        </div>
+    </div>
+
+    
+</x-main>
+
+{{-- @extends('layouts.main')
+
+@section('content')
+
+@endsection --}}
