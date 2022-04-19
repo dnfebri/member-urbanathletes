8466d5ad82778cf56738265f52ae65a84da6eb7b
Unknown-fakh <fakhruddindotcom@gmail.com>
2022-04-19 09:58:16 +0700

    page valor

---
+++ resources/views/public/program/personal-training.blade.php
@@ -0,0 +1,11 @@
+<x-main>
+    <div class="container">
+        ini conten Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, nobis?
+    </div>
+</x-main>
+
+{{-- @extends('layouts.main')
+
+@section('content')
+
+@endsection --}}
--- routes/web/public.php
+++ routes/web/public.php
@@ -1,4 +1,11 @@
 <?php
 use App\Http\Controllers\PublicController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/dev', [PublicController::class, 'home']);
+Route::get('/aboutus', [PublicController::class, 'aboutus'])->name('aboutus');
+Route::get('/terms-conditions', [PublicController::class, 'termsConditions'])->name('termsConditions');
+Route::get('/the-valor', [PublicController::class, 'theValor'])->name('theValor');
+Route::get('/personal-training', [PublicController::class, 'personalTraining'])->name('personalTraining');
+
+
