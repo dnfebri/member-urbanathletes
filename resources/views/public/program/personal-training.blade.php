de348cbfe23d21ec33d813aed6fe5796c7856e74
Unknown-fakh <fakhruddindotcom@gmail.com>
2022-04-19 11:06:50 +0700

    asd

--- resources/views/public/program/personal-training.blade.php
+++ resources/views/public/program/personal-training.blade.php
@@ -1,6 +1,20 @@
 <x-main>
-    <div class="container">
-        ini conten Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, nobis?
+    <div>
+        <img src="{{ url('/image/program/personalTraining/gambar-personal.jpg') }}" alt="pt1">
+    </div>
+
+    <div>
+        <h4 class="text-center text-5xl text-lime-500 my-10">TRY OUR PERSONAL TRAINING PACKAGES</h4>
+    </div>
+
+    <div class="grid md:grid-cols-3 lg:grid-cols-3 gap-0 container justify-center place-items-center">
+        <img class="max-w-xl" src="{{ url('/image/program/personalTraining/body-weight-1.jpg') }}" alt="pt2">
+        <img class="max-w-sm" src="{{ url('/image/program/personalTraining/muscle-gain.jpg') }}" alt="pt3">
+    </div>
+
+    <div>
+        <img src="{{ url('/image/program/personalTraining/strength-and-toning.jpg') }}" alt="pt4">
+        <img src="{{ url('/image/program/personalTraining/body-shaping.jpg') }}" alt="pt5">
     </div>
 </x-main>
 
