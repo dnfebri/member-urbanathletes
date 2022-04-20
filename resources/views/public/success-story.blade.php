9ebe4d4f15b2e925fe4ef3658f25ea5dc8e2ba10
Unknown-fakh <fakhruddindotcom@gmail.com>
2022-04-20 10:47:33 +0700

    media

---
+++ resources/views/public/success-story.blade.php
@@ -0,0 +1,99 @@
+<x-main>
+    <div class="container">
+        <div class="grid md:grid-cols-3 lg:grid-cols-3 gap-7 my-12">
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto1.jpg') }}" alt="ss1">
+                <p class="text-center">JOMARIE ANNE</p>
+                <p class="text-center mb-5">Female, 25th, English Teacher</p>
+                <p>Progress : 12 Maret 2018 – 30 April 2018</p>
+                <p>Trainer   : Dymas</p>
+            </div>
+            
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto2.jpg') }}" alt="ss2">
+                <p class="text-center">YOSEF AUDY</p>
+                <p class="text-center mb-5">Male</p>
+                <p>Progress : 10 Okt 2017 – 10 Maret 2018</p>
+                <p>Trainer   : Dymas</p>
+            </div>
+            
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto3.jpg') }}" alt="ss3">
+                <p class="text-center">NATALIA ARISHANDY</p>
+                <p class="text-center mb-5">Female</p>
+                <p>Progress : 7 Maret 18 – 7 Mei 18</p>
+                <p>Trainer   : Isma</p>
+            </div>
+        </div>
+
+        <div class="grid md:grid-cols-3 lg:grid-cols-3 gap-7 my-12">
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto4.jpg') }}" alt="ss1">
+                <p class="text-center">RANNY</p>
+                <p class="text-center mb-5">Female, 29th</p>
+                <p>Progress : 7 April – 7 Mei</p>
+                <p>Trainer   : Riris</p>
+            </div>
+            
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto5.jpg') }}" alt="ss2">
+                <p class="text-center">BENNY</p>
+                <p class="text-center mb-5">Male</p>
+                <p>Progress : – (7 bulan)</p>
+                <p>Trainer   : Fauzi</p>
+            </div>
+            
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto6.jpg') }}" alt="ss3">
+                <p class="text-center">VERAWATY</p>
+                <p class="text-center mb-5">Female</p>
+                <p>Progress : -</p>
+                <p>Trainer   : -</p>
+            </div>
+        </div>
+
+        <div class="grid md:grid-cols-3 lg:grid-cols-3 gap-7 my-12">
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto7.jpg') }}" alt="ss1">
+                <p class="text-center">RIDZKA</p>
+                <p class="text-center mb-5">Female, 38th</p>
+                <p>Progress : 7 April – 7 Mei</p>
+                <p>Trainer   : Riris</p>
+            </div>
+            
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto8.jpeg') }}" alt="ss2">
+                <p class="text-center">YEN YEN NOERHAJATI GUNAWAN</p>
+                <p class="text-center mb-5">Female, 48th</p>
+                <p>Progress : -</p>
+                <p>Trainer   : Dajan</p>
+            </div>
+            
+            <div>
+                <img class="mb-8" src="{{ url('/image/successStory/foto9.jpg') }}" alt="ss3">
+                <p class="text-center">LORETA FLORENCIA SUSANTO</p>
+                <p class="text-center mb-5">Female</p>
+                <p>Progress : -</p>
+                <p>Trainer   : Sugeng</p>
+            </div>
+        </div>
+
+        <div class="grid md:grid-cols-3 lg:grid-cols-3 gap-7 my-12">
+            <div></div>
+            <div class="">
+                <img class="mb-8" src="{{ url('/image/successStory/foto10.jpg') }}" alt="ss2">
+                <p class="text-center">PURITY SAMBILAN AJI</p>
+                <p class="text-center mb-5">Female, 31th</p>
+                <p>Progress : -</p>
+                <p>Trainer   : Roni</p>
+            </div>
+            <div></div>
+        </div>
+    </div>
+</x-main>
+
+{{-- @extends('layouts.main')
+
+@section('content')
+
+@endsection --}}
