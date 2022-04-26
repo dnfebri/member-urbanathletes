a1842abbcf6eeb144a334084bfa523d774535652
dnfebri <dwinofebri@gmail.com>
2022-04-26 14:43:05 +0700

    setup page Sixpack

--- routes/web/promo.php
+++ routes/web/promo.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\ContentController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\PublicController;
 
-Route::get('/99k', [ContentController::class, 'index99k'])->name('99k');
+Route::get('/99k', function () {return view("public/content/99k");})->name('99k');
+Route::get('/sixpack-3', function () {return view("public/content/sixpack-3");})->name('sixpack-3');
 
 Route::prefix('daftar')->name('daftar.')->group(function () {
   // Route::get('', [PublicController::class, 'daftar'])->name('gym');
