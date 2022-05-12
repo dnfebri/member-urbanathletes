3c187ad4a23820d32c8db563e6743f3efce6ed40
dnfebri <dwinofebri@gmail.com>
2022-05-12 15:48:46 +0700

    fb ads

--- resources/views/public/content/sixpack3/sixpack-3.blade.php
+++ resources/views/public/content/sixpack3/sixpack-3.blade.php
@@ -172,17 +172,12 @@
       t.src=v;s=b.getElementsByTagName(e)[0];
       s.parentNode.insertBefore(t,s)}(window, document,'script',
       'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '544863203544959');
+      fbq('init', '370181471818627');
       fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=544863203544959&ev=PageView&noscript=1"
+    src="https://www.facebook.com/tr?id=370181471818627&ev=PageView&noscript=1"
     /></noscript>
-    <script type="text/javascript">
-      document.getElementById('addToCartButton').addEventListener('click', function() {
-      fbq('track', 'AddToCart');
-      }, false);
-    </script>
     <!-- End Meta Pixel Code -->
     
   @stack('script')
