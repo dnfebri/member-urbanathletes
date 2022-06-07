646ed0c9ada408cd5a91f0c966f704a0873c4a10
Urban Athletes <93457663+urbanathletes@users.noreply.github.com>
2022-06-07 14:12:43 +0700

    https Jquery

--- resources/views/public/member/daftar/daftar_confirm.blade.php
+++ resources/views/public/member/daftar/daftar_confirm.blade.php
@@ -2,7 +2,7 @@
   @push('style')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
     
   @endpush
